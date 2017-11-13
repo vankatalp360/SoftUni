31127c2dc865277f03e7d09b3353564832bc4a84
StanislavStefanovPetrov <eqsol.sofia@gmail.com>
2017-11-13 21:45:45 +0200

    Add some items

---
+++ 05 Databases Advanced - Entity Framework/14 EXERCISE ENTITY RELATIONS/P03_FootballBetting/P03_FootballBetting/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_FootballBetting.Data
+{
+    class Configuration
+    {
+        public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=FootballBettingContext;Integrated Security=true";
+    }
+}
---
+++ 05 Databases Advanced - Entity Framework/14 EXERCISE ENTITY RELATIONS/P03_FootballBetting/P03_StartUp_FootballBetting/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace P03_FootballBetting
+{
+    using System;
+    using P03_FootballBetting.Data;
+    using Microsoft.EntityFrameworkCore;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new FootballBettingContext();
+
+            context.Database.EnsureDeleted();
+
+            context.Database.Migrate();
+
+            Console.WriteLine();
+        }
+    }
+}
